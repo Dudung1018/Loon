2edda6f527e948bac973f74e14d89512b0b79e52
Dudung1018 <lilei112500@gmail.com>
2025-08-10 23:52:00 +0800

    新增香蕉视频会员解锁脚本-surge

--- xjsp.js
+++ xjsp.js
@@ -22,11 +22,11 @@ let url = $request.url
 
 if (url.indexOf('/ucp/index') !== -1){
     let data = JSON.parse(body)
-    let json = JSON.parse(data["data"])
+    let json = data["data"]
     json.user.username = '小白解锁'
     json.user.nickname = '小白解锁'
     json.user.isVip = 1
-    data["data"] = JSON.stringify(json)
+    data["data"] =json
     $.done({body :JSON.stringify(data)})
 }
 function Env(a, b) {
