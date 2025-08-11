fcb9a35c0ed422bf410fb03c600894deeec216d9
Dudung1018 <lilei112500@gmail.com>
2025-08-11 14:53:00 +0800

    新增 cute 计划修改-test

--- cute.js
+++ cute.js
@@ -4,7 +4,7 @@ const requestUrl = $request.url;
 
 if(requestUrl.indexOf('/user/info') !== -1) {
     const data = JSON.parse(body);
-    data.data.plan_id = 9;
+    data.data.plan_id = 50;
     $.done({body: JSON.stringify(data)})
 }
 
