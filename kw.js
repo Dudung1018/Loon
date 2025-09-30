a1a1dd643361629b08b24f12288b8fa30435969a
Dudung1018 <lilei112500@gmail.com>
2025-10-01 00:43:48 +0800

    酷我标准版

--- kw.js
+++ kw.js
@@ -32,7 +32,7 @@ if (url.includes("/music.pay") && method == "POST") {
             let obj = JSON.parse(data);
             obj.songs[0].audio.forEach((item) => (item.st = 0))
             let br = obj.songs[0].audio.find(item => item.quality === quality).br
-            if(br===20000) br = br+'mflac'
+            if(br===2000) br = br+'mflac'
             else br = br +'kmp3'
             $persistentStore.write(obj.songs[0].id, "rid");
             $persistentStore.write(br,"br")
